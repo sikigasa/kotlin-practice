30eff50b9ffa47b787b2196ef86221c9b69a0f97
sikigasa <km3t1114@yahoo.co.jp>
2024-05-11 16:06:19 +0900

    add: OnboardingPreview

--- app/src/main/java/com/example/firstkotlin/MainActivity.kt
+++ app/src/main/java/com/example/firstkotlin/MainActivity.kt
@@ -3,17 +3,22 @@ package com.example.firstkotlin
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -79,5 +84,29 @@ fun Greeting(
 fun GreetingPreview() {
     FirstKotlinTheme {
         MyApp()
+    }
+}
+
+@Composable
+fun OnBoardingScreen(modifier: Modifier = Modifier) {
+    //
+    var shouldShowOnBoarding by remember { mutableStateOf(true) }
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        Text("Welcome to the Basics Codelab!")
+        Button(modifier = Modifier.padding(vertical = 24.dp), onClick = { shouldShowOnBoarding = false }) {
+            Text("Continue")
+        }
+    }
+}
+
+@Preview(showBackground = true, widthDp = 320, heightDp = 320)
+@Composable
+fun OnboardingPreview() {
+    FirstKotlinTheme {
+        OnBoardingScreen()
     }
 }
